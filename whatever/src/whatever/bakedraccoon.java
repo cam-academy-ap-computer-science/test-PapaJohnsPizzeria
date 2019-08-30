8c57a0848ac044045d062feded0f751275660495
almajjoh000 <almajjoh000@CAM-LAB-08.bgsd.k12.wa.us>
2019-08-30 07:28:21 -0700

    modified version of HelloWorld.

--- whatever/src/whatever/bakedraccoon.java
+++ whatever/src/whatever/bakedraccoon.java
@@ -4,7 +4,7 @@ public class bakedraccoon {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Baked Raccoons be feeding...");
+		System.out.println("Baked raccoons be feeding... on something mildly appropriate!");
 	}
 
 }
