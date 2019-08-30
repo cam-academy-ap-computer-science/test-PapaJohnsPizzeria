711bf81c76eab433804e3ea79fbf812137a10619
almajjoh000 <almajjoh000@CAM-LAB-08.bgsd.k12.wa.us>
2019-08-30 07:19:03 -0700

    New BakedRaccoon project.

---
+++ whatever/src/whatever/bakedraccoon.java
@@ -0,0 +1,10 @@
+package whatever;
+
+public class bakedraccoon {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!!!");
+	}
+
+}
